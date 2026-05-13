0ac0642a025e406dd8edb65c2e1414a6d8989075
Guilherme Brogio Macedo da Silva <79169549+gbrogio@users.noreply.github.com>
2026-05-13 00:28:32 -0300

    Revert "feat(front): add theme provider and design tokens for colors, borders…"
    This reverts commit 5d7750c32768e7460476b23312365ebea733cfc0.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { ThemeProvider } from './providers/ThemeProvider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>{null}</ThemeProvider>
+
   </StrictMode>,
 )
