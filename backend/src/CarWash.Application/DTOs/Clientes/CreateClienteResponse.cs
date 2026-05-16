2a32e358f5b149411d563c16df5be03c472bf75f
Guilherme Brogio Macedo da Silva <79169549+gbrogio@users.noreply.github.com>
2026-05-16 14:57:01 -0300

    feat(cliente): RF003 validações de limites e formatos em cima da arquitetura nova (#19)
    Co-authored-by: Lucas Arruda <168601194+LucasUlissesArruda@users.noreply.github.com>
    Co-authored-by: TomaziVinicius <viniciustomazi0712@gmail.com>

--- backend/src/CarWash.Application/DTOs/Clientes/CreateClienteResponse.cs
+++ backend/src/CarWash.Application/DTOs/Clientes/CreateClienteResponse.cs
@@ -4,7 +4,7 @@ public class CreateClienteResponse
 {
     public Guid Id { get; set; }
 
-    public string Mensagem { get; set; } = "Cliente Cadastrado";
+    public string Mensagem { get; set; } = "Dados do cliente validados e salvos com sucesso!";
 
     public string TraceId { get; set; } = string.Empty;
 }
