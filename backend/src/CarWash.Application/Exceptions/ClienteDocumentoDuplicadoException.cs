0a4d1231c2df92f9aea873f49a417a2db978a0f8
Guilherme Brogio Macedo da Silva <79169549+gbrogio@users.noreply.github.com>
2026-05-25 00:48:43 -0300

    Revert "Feat/129 rf021 adicionar veículo no fluxo de cadastro de cliente" (#80)

--- backend/src/CarWash.Application/Exceptions/ClienteDocumentoDuplicadoException.cs
+++
@@ -1,9 +0,0 @@
-namespace CarWash.Application.Exceptions;
-
-public class ClienteDocumentoDuplicadoException : Exception
-{
-    public ClienteDocumentoDuplicadoException()
-        : base("Já existe cliente cadastrado com este documento.")
-    {
-    }
-}
