3163074218866fd55e8021c275deb08319e7b63a
TomaziVinicius <viniciustomazi0712@gmail.com>
2026-05-25 00:36:20 -0300

    feat(clientes): adicionar veiculos no cadastro de cliente (#79)

---
+++ backend/src/CarWash.Application/DTOs/Clientes/CreateClienteResponse.cs
@@ -0,0 +1,8 @@
+namespace CarWash.Application.DTOs.Clientes;
+
+public class CreateClienteResponse{
+    public Guid Id{ get; set;}
+    public string Mensagem {get; set; } = "Dados do cliente e veiculos validados e salvos com sucesso!";
+    public string TraceId { get; set; } = string.Empty;
+    public List<VeiculoCriadoResponse> Veiculos { get; set; } = [];
+}
